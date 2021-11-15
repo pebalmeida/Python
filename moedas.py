ccbd773dd63d06ac13128aff6afd20b20b003990
pebalmeida <93488924+pebalmeida@users.noreply.github.com>
2021-11-15 11:59:44 -0300

    Add files via upload

---
+++ moedas.py
@@ -0,0 +1,6 @@
+real = float(input('Quanto dinheiro voce tem na carteira ?'))
+real = float(input('Quanto dinheiro voce tem na carteira ?'))
+dolar = real / 3.27
+euro = real / 0.16
+print('Com R${} voce pode comprar US${}'.format(real, dolar))
+print('Com R${} voce pode comprar EUR${}'.format(real, euro))
